c8ed7254a5b154bd92c13f37be5b8a95ad2515c1
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-16 08:29:49 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 int main()
 {
-	char a[]=""""Programming is like building a multilingual puzzle";
-	puts(a);
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0 ;
 }
