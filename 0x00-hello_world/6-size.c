4c746ce856a1e0c1a42833772752d416ac799e0c
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-16 11:04:21 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,8 +12,9 @@ int main(void)
 
 	printf("Size of a char: %zu byte(s)",sizeof(b));
 	printf("Size of an int: %zu byte(s)",sizeof(a));
+	printf("Size of a long int: %zu byte(s)",sizeof(a));
+	printf("Size of a long long int: %zu byte(s)",sizeof(d));
 	printf("Size of a float: %zu byte(s)",sizeof(c));
-	printf("Size of a double: %zu byte(s)",sizeof(d));
 	return (0);
 }
 
