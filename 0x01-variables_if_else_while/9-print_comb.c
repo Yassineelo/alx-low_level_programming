d8fe6d2f1ae2431805ceac3516f442e70c6a0dad
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-17 15:44:58 -0700

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - combination of single digits
+ * Return:0 (Success)
+ */
+
+int main()
+{
+	int i;
+	int j;
+
+	i == 0;
+	j == 0;
+	while (i < 10)
+	{
+		while (j < 10)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 9 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		i++;
+		}
+	}
+	return (0);
+}
