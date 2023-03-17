1653efa481abe16eab09c94edcaceede35417d63
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-16 17:20:50 -0700

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Print string on the standard error
+ * return=1 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
