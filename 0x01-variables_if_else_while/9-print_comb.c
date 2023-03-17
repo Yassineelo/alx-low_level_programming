2f8cdbd5e4c999a4b8c99ce3aa38adcd3a4ae736
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-17 16:26:44 -0700

    combination

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,19 +7,14 @@
 int main()
 {
 	int i;
-	int j;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i);
-			putchar(j);
-			if (i < 9 || j < 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	return (0);
