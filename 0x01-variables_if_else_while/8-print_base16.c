fb1d127661a7a1d942bf09b7fb14af6654b939e3
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-17 14:36:24 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Numbers in lowercase
+ * Return:0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	i = 0;
+	while (i < 16)
+		if (i < 10)
+		{
+			putchar(i + '0');
+			i++;
+		}
+		else
+		{
+			putchar(i - 10 + 'a');
+			i++;
+		}
+	putchar('\n');
+	return (0);
+}
