4504a85e8ac3c7fa6cd418a1b791dcb56afa3d00
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-18 04:32:01 -0700

    print combination two two digits

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,7 +4,7 @@
  * Return:0 (Success)
  */
 
-int main()
+int main(void)
 {
 	int i;
 	int j;
@@ -12,25 +12,25 @@ int main()
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
+		{
+			int a = i / 10;
+			int b  = i % 10;
+			int c = j / 10;
+			int d = j % 10;
+			if (i < j)
 			{
-				int a = i / 10;
-				int b  = i % 10;
-				int c = j / 10;
-				int d = j % 10;
-				if (i < j)
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(' ');
+				putchar(c + '0');
+				putchar(d + '0');
+				if (i != 98 || j != 99)
 				{
-					putchar(a + '0');
-					putchar(b + '0');
+					putchar(',');
 					putchar(' ');
-					putchar(c + '0');
-					putchar(d + '0');
-					if (i != 98 || j != 99)
-					{
-						putchar(',');
-						putchar(' ');
-					}
 				}
 			}
+		}
 	}
 	return (0);
 }
