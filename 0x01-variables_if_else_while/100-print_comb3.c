3c9c5943f48cde15039907c6b0529a7394a21db0
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-18 02:04:20 -0700

    combination two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,15 @@ int main(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8 || j != 9)
+			if (i < j)
 			{
-				putchar(',');
-				putchar(' ');
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i != 8 || j != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
