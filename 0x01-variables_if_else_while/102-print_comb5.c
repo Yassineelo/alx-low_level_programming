80e6c6ad27fa40a02d16dde55ada28bc387d33c2
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-18 04:26:59 -0700

    print combination two two digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - combination of single digits
+ * Return:0 (Success)
+ */
+
+int main()
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = 0; j < 100; j++)
+			{
+				int a = i / 10;
+				int b  = i % 10;
+				int c = j / 10;
+				int d = j % 10;
+				if (i < j)
+				{
+					putchar(a + '0');
+					putchar(b + '0');
+					putchar(' ');
+					putchar(c + '0');
+					putchar(d + '0');
+					if (i != 99 || j != 99)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+	}
+	return (0);
+}
