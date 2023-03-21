b1e15934b8ef85fb479d031fff7bf52038056719
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-21 12:24:44 -0700

    print alphabets in 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print in 10 times the alphabets
+ * return : 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+	
+	i = 1;
+	while (i <= 10)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+		i++;
+	}
+}
