d05e5aca0d22adf89f5e29b3a3484bcdad6bba76
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-22 16:01:46 -0700

    is alpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - alphabetic character
+ * Return: 1 if alphabetic or 0 if others
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
