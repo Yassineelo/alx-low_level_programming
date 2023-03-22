d7742015b7997e33f59802bf70ca036085c6964d
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-22 16:12:36 -0700

    sign of numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of number
+ * @n: for check
+ * return: 1 if positive and -1 if negative and 0 for others
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
