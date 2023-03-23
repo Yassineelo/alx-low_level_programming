73d1ba778c2d345035b88106921b999ce062cbc3
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-23 07:59:13 -0700

    add readme file and check for upercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - check for upercase character
+ * @c : character to be checked
+ * Return: 1 if upercase, 0 for otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return(1);
+	}
+	return(0);
+
+}
