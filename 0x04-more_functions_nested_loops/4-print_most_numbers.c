aebc6a27ae125da5fd19598ebf19731270e9e90a
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-25 03:14:54 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,15 +9,14 @@ void print_most_numbers(void)
 {
 	int i = 0;
 
-	do
+	while (i <= 9)
 	{
-		if (i != 2 || i != 4)
+		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 			i++;
 		}
 	}
-	while (i <= 9);
 	_putchar('\n');
 }
 
