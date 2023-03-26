6355a3d7e7aef7fce026071fa55b9236d73cc22f
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-26 10:28:28 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle withing #
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i, j, y;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i <= size; i++)
+		{
+			for (j = size - i; j > 1; j++)
+			{
+				for (y = 0; y <= i; y++)
+				{
+					_putchar(35);
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
