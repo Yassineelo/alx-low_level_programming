04fd3eeac7d85fa191b8ce0cb01c617cc23c5ec5
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-27 09:08:58 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+/**
+ * _strlen - length of string
+ * @s : string to return length
+ * Return : length of 's'
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
+
+
