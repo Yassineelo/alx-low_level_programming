c7953a99601d9b770729e6037469f0712e2c5c7e
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-27 13:10:08 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 /**
- * _puts
+ * _puts - print a string
+ * @str: string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
