0ed2aaf913f4110fc691eb4a7f3d9f59f66a2044
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-27 18:01:43 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	char reverse = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+	for (i = 0; i < c; i++)
+	{
+		c--;
+		reverse = s[i];
+		s[i] = s[c];
+		s[c] = reverse;
+	}
+}
