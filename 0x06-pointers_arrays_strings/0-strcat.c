4795e0bff21c318cb4a7e5322c1a392e62c610df
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-29 08:38:00 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenate two strings
+ * @src: source
+ * @dest: destination
+ * Return: resultin string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return(0);
+}
