d9c40c6964814d64ed80f8b57058a095559503a0
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-29 09:45:04 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcmp - compare two strings
+ * @s1: to be comapred with s2
+ * @s2: to be compared with s1
+ * Return: 0
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int result = strcmp(s1, s2);
+	return (result);
+}
