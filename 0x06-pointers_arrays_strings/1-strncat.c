f20f81b95bf7acebfd21f2430bd1f1819811fbba
Yassine Elouazzani <elouazzani1@gmail.com>
2023-03-29 08:48:17 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenate two strings using n bytes from src
+ * @src: source
+ * @dest: destination
+ * @n: number of characters to concatenate
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	strncat(dest, src, n);
+	return (dest);
+}
