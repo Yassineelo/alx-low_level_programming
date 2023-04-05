1bb666f8f8a4c1d7eeb5e7ef1b38a87bcaf73e7d
Yassine Elouazzani <elouazzani1@gmail.com>
2023-04-05 06:40:02 -0700

    task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen_recursion - entry point
+ * @s: string
+ * Return: length of string
+ */
+
+int _strlen_recursion(char *s)
+{
+	char res = strlen(s);
+
+	return (res);
+}
