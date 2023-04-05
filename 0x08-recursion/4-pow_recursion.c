ea3a9d0f3331f5a668ec42a405dd6176b5e8631f
Yassine Elouazzani <elouazzani1@gmail.com>
2023-04-05 09:56:12 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * _pow_recursion - entry point
+ * @x: input
+ * @y: input
+ * Return: 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (x* _pow_recursion(x, y - 1));
+}
