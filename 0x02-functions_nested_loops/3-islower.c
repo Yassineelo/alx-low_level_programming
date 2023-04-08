46ffe3b7222223517693c232b31d268af851165d
Yassine Elouazzani <elouazzani1@gmail.com>
2023-04-08 05:44:51 -0700

    task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - check lowercase
+ * @c: input
  * return: 1 is lowercases and 0 is others
  */
 
@@ -8,7 +9,7 @@ int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
 	{
-		return(1);
+		return (1);
 	}
-	return(0);
+	return (0);
 }
