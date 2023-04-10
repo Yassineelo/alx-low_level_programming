efcf83fbdd6317189096408fcbb95fe8ab8a5480
Yassine Elouazzani <elouazzani1@gmail.com>
2023-04-10 07:47:20 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argc
+ * @argv: argv
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", *argv);
+	return (0);
+}
