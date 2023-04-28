472adee2cda52f530219feb9eb49f442c2d1e7a3
Yassine Elouazzani <elouazzani1@gmail.com>
2023-04-28 16:42:01 -0700

    task 1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,18 +3,17 @@
 
 /**
  * list_len - Entry point
- * @h: h 
+ * @h: h
  * Return: number of elements in h
  */
 size_t list_len(const list_t *h)
 {
- size_t n = 0;
-
- while (h)
- {
- n++;
- h = h->next;
- }
- return (n);
+size_t n = 0;
+while (h)
+{
+n++;
+h = h->next;
+}
+return (n);
 }
 
