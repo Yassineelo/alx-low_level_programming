b4723803a30a654a8e14a090f11d78f9014e729c
Yassine Elouazzani <elouazzani1@gmail.com>
2023-07-06 16:33:59 -0700

    task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - task 5
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a, c = 0;
+	unsigned long int exc = n ^ m;
+        unsigned long int curr;
+
+	for (a = 63; a >= 0; a--)
+	{
+		curr = exc >> a;
+		if (curr & 1)
+			c++;
+	}
+
+	return (c);
+}
+
